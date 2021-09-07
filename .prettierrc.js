7fbb58e4980f07e428a7f41f8180433a43a8aa19
daibergm <daibergonzalez@gmail.com>
2021-09-07 10:06:39 -0500

    Configured eslint and prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
+  bracketSpacing: true,
   jsxBracketSameLine: true,
+  semi: true,
   singleQuote: true,
   trailingComma: 'all',
+  endOfLine: 'auto',
   arrowParens: 'avoid',
 };
