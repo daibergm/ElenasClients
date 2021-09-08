c3823310753ac5aa28732cef5b3cf06baaef35a7
daibergm <daibergonzalez@gmail.com>
2021-09-08 14:31:51 -0500

    Added loading component

---
+++ src/components/common/Loading/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import { Colors } from '../../../assets';
+
+export default StyleSheet.create({
+  container: {
+    position: 'absolute',
+    backgroundColor: Colors.white,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+  },
+});
