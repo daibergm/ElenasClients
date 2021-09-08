093c58255aaf74ea74ee19e1f15a69dbd521e7bb
daibergm <daibergonzalez@gmail.com>
2021-09-07 22:35:18 -0500

    Added automatic logout

--- src/screens/Clients/ClientsScreen.tsx
+++ src/screens/Clients/ClientsScreen.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { SafeAreaView, Text } from 'react-native';
+
+// @Context
+import { AccountContext } from '../../context/';
 
 // @Styles
 import styles from './styles';
 
 function ClientsScreen() {
+  // TODO: Remove when implement drawer navigator
+  const { onLogout } = useContext(AccountContext);
+
+  // TODO: Remove when implement drawer navigator
+  useEffect(() => {
+    setTimeout(() => {
+      onLogout && onLogout();
+    }, 2000);
+  }, [onLogout]);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Lista de clientes</Text>
